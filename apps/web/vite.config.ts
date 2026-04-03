066059f923b1ff0621e41ab4a973cdf0d95a523f
matheuscamposmt <matheuscamposmattos@id.uff.br>
2026-04-03 15:52:17 -0300

    hotfix: add railway vite config allowed hosts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -10,6 +10,6 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ['.trycloudflare.com']
+    allowedHosts: ['.trycloudflare.com', 'web-production-bbe3.up.railway.app']
   }
 })
